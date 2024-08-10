3c458ffe9239a9b2fb140f802d3d9fa0ca10d384
Roman Kuzmin <ghelatin@mail.ru>
2024-08-10 10:40:29 +0300

    Home Work done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        LoginService loginService = new LoginService();
 
+        String login = "Iam_Deer2";
+        String password = "BlaBlaDer100";
+        String confirmPassword = "BlaBlaDer100";
+        try {
+            loginService.check(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка логина: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка пароля: " + e.getMessage());
+        } finally {
+            System.out.println("Обработка данных завершена");
+        }
     }
 }
